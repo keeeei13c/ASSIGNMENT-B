c54cddc0aede444f128f0ea4d86f912280341b62
keeeei13c <kei.121003f@icloud.com>
2021-07-13 23:06:32 +0900

    change setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  webpack5: false,
+  // webpack5: false,
   env: {
     GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
   },
