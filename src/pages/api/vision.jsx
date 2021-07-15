07f4aac193b483bb59045c545adcacc3ff6452a6
keeeei13c <kei.121003f@icloud.com>
2021-07-15 22:45:27 +0900

    change

--- src/pages/api/vision.jsx
+++ src/pages/api/vision.jsx
@@ -3,8 +3,11 @@ import vision from '@google-cloud/vision'
 const client = new vision.ImageAnnotatorClient()
 
 export default async function handler(req, res) {
-  vision: process.env.GOOGLE_APPLICATION_CREDENTIALS
   const url = req.body.url
   const [result] = await client.labelDetection(url)
-  res.status(200).json({ data: result.labelAnnotations})
+  res.status(200).json({ data: result.labelAnnotations })
+  
+  return (
+    <h1>hello</h1>
+  )
 }
